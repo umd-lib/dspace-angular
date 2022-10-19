4691b66a897dd5ec1715ff489769298dbfc2aa19
David P. Steelman <dsteelma@umd.edu>
2022-10-19 12:24:01 -0400

    LIBDRUM-676. Added tests for unit-groups-list-component.ts
    Added tests for "unit-groups-list-component.ts", using tests in
    src/app/access-control/group-registry/group-form/members-list/members-list.component.spec.ts
    as examples.
    
    Unfortunately, some of the tests in "members-list.component.spec.ts"
    were passing "trivially", with a "SPEC HAS NO EXPECTATIONS" warnings,
    indicating that no test expectations were encountered when running the
    test. Where possible, these warnings were fixed in the unit-groups-list
    implementation. Tests from the example that did not have readily
    discernible fixes were not included.
    
    https://issues.umd.edu/browse/LIBDRUM-676

--- src/app/shared/testing/unit-mock.ts
+++ src/app/shared/testing/unit-mock.ts
@@ -1,4 +1,5 @@
 import { Unit } from 'src/app/core/eperson/models/unit.model';
+import { GroupMock } from './group-mock';
 
 export const UnitMock: Unit = Object.assign(new Unit(), {
   _name: 'testunitname',
@@ -23,5 +24,19 @@ export const UnitMock2: Unit = Object.assign(new Unit(), {
         href: 'https://rest.api/server/api/eperson/units/testunitid2',
     },
     groups: {  },
+  },
+});
+
+export const UnitMockWithGroup: Unit = Object.assign(new Unit(), {
+  _name: 'testunitid3_with_group',
+  id: 'testunitid3_with_group',
+  uuid: 'testunitid3_with_group',
+  type: 'unit',
+  groups: [GroupMock],
+  _links: {
+    self: {
+        href: 'https://rest.api/server/api/eperson/units/testunitid3_with_group',
+    },
+    groups: { href: 'https://rest.api/server/api/eperson/units/testunitid3_with_group/groups' },
   },
 });
