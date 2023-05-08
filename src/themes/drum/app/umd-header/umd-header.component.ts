38ef17220b8a8ebd27844e0e8fbfbf7dd42bc8f1
David P. Steelman <dsteelma@umd.edu>
2023-05-08 12:10:44 -0400

    LIBDRUM-786. Fix ReferenceError in UMD Header component
    Fixed a "ReferenceError: document is not defined" error in the log by
    verifying that the "document" object in the UmdHeaderComponent is
    defined and non-null,
    
    https://umd-dit.atlassian.net/browse/LIBDRUM-786

--- src/themes/drum/app/umd-header/umd-header.component.ts
+++ src/themes/drum/app/umd-header/umd-header.component.ts
@@ -15,7 +15,8 @@ export class UmdHeaderComponent implements OnInit {
   ngOnInit() {
     const url = 'https://umd-header.umd.edu/build/bundle.js?search=0&search_domain=&events=0&news=0&schools=0&admissions=0&support=1&support_url=https%253A%252F%252Fgiving.umd.edu%252Fgiving%252FshowSchool.php%253Fname%253Dlibraries&wrapper=1160&sticky=0';
     // check for an existing #umdheader-main element, so we only add the header once
-    if (document.getElementById('umdheader-main') === null) {
+    if ((typeof document !== 'undefined') && (document != null) &&
+         document.getElementById('umdheader-main') === null) {
       const node = document.createElement('script');
       node.src = url;
       document.body.appendChild(node);
