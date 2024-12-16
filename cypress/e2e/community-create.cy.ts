3e52fcee459b9faa05d8bb3b8ebc065e7cf237c6
David P. Steelman <dsteelma@umd.edu>
2024-12-16 10:34:34 -0500

    LIBDRUM-892. Skip "community-create-cy.ts" Cypress e2e test
    This test was failing with the following:
    
    ```
    cypressError: `cy.type()` failed because it targeted a disabled element.
    ```
    
    This is because the community form has been modified by UMD to include
    a required "Community Group" field, that cannot be populated from the
    stock DSpace database used by the e2e tests when run by GitHub actions.
    
    Skipping the test so that GitHub Actions is can still succeed.
    
    https://umd-dit.atlassian.net/browse/LIBDRUM-892

--- cypress/e2e/community-create.cy.ts
+++ cypress/e2e/community-create.cy.ts
@@ -3,7 +3,13 @@ beforeEach(() => {
   cy.loginViaForm(Cypress.env('DSPACE_TEST_ADMIN_USER'), Cypress.env('DSPACE_TEST_ADMIN_PASSWORD'));
 });
 
-it('should show loading component while saving', () => {
+// UMD Customization
+// The dspace backend in docker-compose-ci.yml uses stock dspace image, so the
+// communitygroups endpoint used by /community-list page is not available.
+// The default database restore SQL used by docker-compose-ci.yml does not
+// include the CommuityGroup info, therefore this test will fail, because
+// the form cannot be submitted.
+it.skip('should show loading component while saving', () => {
   const title = 'Test Community Title';
   cy.get('#title').type(title);
 
@@ -11,3 +17,4 @@ it('should show loading component while saving', () => {
 
   cy.get('ds-loading').should('be.visible');
 });
+// End UMD Customization
